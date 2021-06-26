6971c9af7b95c21846ce51e9301755caaff8a3b5
rahul30rajendran <84763801+rahul30rajendran@users.noreply.github.com>
2021-06-26 18:39:17 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+void swap_max(int arr[100],int n,int a)
+  {
+  int maxi=-18889;
+  for (int i=a;i<n;i++)
+  {if(arr[i]>maxi)
+  maxi=arr[i];
+  int temp=arr[a];;
+  arr[a]=maxi;
+  arr[i]=temp;
+  }
+  }
+  void sort(int arr[100],int n)
+{for(int i=0;i<n-1;i++)
+swap_max(arr,n,i);
+}
